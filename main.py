2c6c5b4aaa07776a42ca3167a2bcfdab51b3227e
lastre <orionbl@hotmail.es>
2016-12-09 11:00:10 +0100

    [Base] add crosshair

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ class Game:
         self.main(screen)
 
     def main (self, screen):
+        self.screen = screen
+
         MAIN_LOOP = True
         FPS = 60
         BG_COLOR = (200, 200, 200)
 
         clock = pygame.time.Clock() #for using less CPU, events are queued, may miss
         
-        sprites = pygame.sprite.Group()
+        sprites = pygame.sprite.OrderedUpdates()
         self.target = Target(sprites)
-        
+        sprites.add(self.loadCrosshair())
+
         self.toggleCaptureMouse()
 
         while MAIN_LOOP:
@@ -38,6 +41,18 @@ class Game:
                 sprites.draw(screen)
                 pygame.display.flip() #tearing, double buffering->display buffer/drawing bugffer
             print("\r %i     "%int(clock.get_fps()), end='')
+
+    def loadCrosshair (self):
+        crosshairSize = (32, 32)
+        center = ((self.screen.get_width()-crosshairSize[0])/2, (self.screen.get_height()-crosshairSize[1])/2)
+        print(center)
+        crosshair = pygame.sprite.Group()
+        crosshairImg = pygame.image.load("./img/crosshair.png")
+        crosshairImg = pygame.transform.scale(crosshairImg, crosshairSize)
+        crosshairSprite = pygame.sprite.Sprite(crosshair)
+        crosshairSprite.image = crosshairImg
+        crosshairSprite.rect = pygame.rect.Rect(center, crosshairSize)
+        return crosshair
 
     def toggleCaptureMouse (self):
         self.isMouseCaptured = not self.isMouseCaptured 
@@ -63,9 +78,8 @@ class Target (pygame.sprite.Sprite):
     def shoot (self):
        center =  (self.rect.x + self.radius, self.rect.y + self.radius)
        print(center)
-       
 
-    
+
 if __name__ == '__main__':
     pygame.init()
     print(pygame.display.Info())
