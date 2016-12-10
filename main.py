2f96b248b568b3c9891e06389a9dd511d62493d4
lastre <orionbl@hotmail.es>
2016-12-10 11:23:00 +0100

    [Stats] add stats class and calculations

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pygame
 from random import uniform as random
 
 images = {}
-
 sounds = {}
 
 class Game:
     def __init__ (self, screen):
         self.isMouseCaptured = False
         self.screen = screen
+        self.stats = Statistics()
 
         global images, sounds
 
@@ -30,12 +30,15 @@ class Game:
         MAIN_LOOP = True
         FPS = 60
         BG_COLOR = (200, 200, 200)
+        NUM_TARGETS = 2
 
         clock = pygame.time.Clock() #for using less CPU, events are queued, may miss
         
         sprites = pygame.sprite.OrderedUpdates()
         self.background = Background(self.screen)
-        self.target = Target(sprites, self.screen)
+        self.targets = []
+        for i in range(NUM_TARGETS):
+            self.targets.append(Target(sprites, self.screen))
         sprites.add(self.loadCrosshair())
         self.toggleCaptureMouse()
 
@@ -50,10 +53,13 @@ class Game:
             sprites.update(dt, self)#groups
             sprites.draw(self.screen)
             pygame.display.flip() #tearing, double buffering->display buffer/drawing bugffer
-            #print("\r %i     "%int(clock.get_fps()), end='')
+
+            print("\r FPS:%.2f  ACC:%.2f%%  SPD:%.2f HIT:%i  ESC:%i    " % 
+                (clock.get_fps(), self.stats.get_accuracy(), self.stats.get_speed(), self.stats.hits, self.stats.escapes), 
+                end='')
 
     def exit (self):
-        pass
+        print('')
 
     def gameEvent (self):
         for event in pygame.event.get():
@@ -82,6 +88,7 @@ class Game:
         pygame.mouse.set_visible(not self.isMouseCaptured) 
         pygame.event.set_grab(self.isMouseCaptured)
 
+
 class Target (pygame.sprite.Sprite):
     def __init__ (self, *groups):
         super(Target, self).__init__(groups[0])
@@ -93,8 +100,13 @@ class Target (pygame.sprite.Sprite):
         self.radius2 = self.radius**2 
         self.image = pygame.transform.scale(self.image, (self.radius*2, self.radius*2))
         self.rect = pygame.rect.Rect((-self.radius*2, -self.radius*2), (self.radius*2, self.radius*2))
-        self.timeToLive = 1500
+        self.timeToLive = 2000
         self.timeLived = 0
+
+        self.hGenRange = 0.1
+        self.vGenRange = 0.25
+        self.hGen = (self.screen.get_width()*self.hGenRange, self.screen.get_width()*(1-self.hGenRange))
+        self.vGen = (self.screen.get_height()*self.vGenRange, self.screen.get_height()*(1-self.vGenRange))
 
     def update (self, dt, game):
         self.timeLived += dt
@@ -106,6 +118,7 @@ class Target (pygame.sprite.Sprite):
             self.shoot()
 
     def shoot (self):
+        game.stats.shoot()
         self.shootST.stop()
         self.shootST.play()
         center = (self.rect.x + self.radius - self.screen.get_width()/2, self.rect.y + self.radius - self.screen.get_height()/2)
@@ -115,10 +128,11 @@ class Target (pygame.sprite.Sprite):
 
     def respawn (self):
         self.timeLived = 0
-        self.rect.x = random(0, self.screen.get_width())
-        self.rect.y = random(0, self.screen.get_height())
+        self.rect.x = random(*self.hGen)
+        self.rect.y = random(*self.vGen)
 
     def miss (self):
+        game.stats.escape()
         self.missST.stop()
         self.missST.play()
         self.respawn()
@@ -126,7 +140,9 @@ class Target (pygame.sprite.Sprite):
     def hit(self):
         #self.hitST.stop()
         #self.hitST.play()
+        game.stats.hit(self.timeLived)
         self.respawn()
+
 
 class Background:
     def __init__ (self, screen):
@@ -146,6 +162,40 @@ class Background:
         for i in range(self.columns):
             for j in range(self.rows):
                 game.screen.blit(self.image, ((i-1)*self.size+self.xPos, (j-1)*self.size+self.yPos))
+
+
+class Statistics:
+    def __init__ (self):
+        self.shots = 0
+        self.hits = 0
+        self.escapes = 0
+        self.speed = 0
+        self.accuracy = []
+        self.isStarted = False
+
+    def shoot (self):
+        self.shots += 1
+
+    def escape (self):
+        if self.isStarted:
+            self.escapes += 1
+
+    def hit (self, speed=0):
+        if not self.isStarted:
+            self.isStarted = True
+        self.hits += 1
+        self.speed += speed
+
+    def get_accuracy (self):
+        if self.shots:
+            return (self.hits/self.shots)*100
+        return 0
+
+    def get_speed (self):
+        if self.hits:
+            return (self.speed / self.hits)/1000
+        return 0
+
 
 if __name__ == '__main__':
     pygame.init()
